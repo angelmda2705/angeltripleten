804421d4e57c6dddf5594a3a25369fa12b412927
Angel Mendiola <angelmendiolalux@gmail.com>
2025-12-01 18:32:43 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+
+st.header("lanzar una moneda")
+
+number_of_trials = st.slider("¿Numero de intentos?", 1, 1000, 10)
+
+start_button = st.button("Ejecutar")
+
+if start_button :
+    st.write ( f" Experimento con {number_of_trials} intentos en curso.")
+st.write("Esta aplicacion aun no es funcional. En construccion.")
